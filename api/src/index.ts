639d08a15c519ef28b7f95711766b6a68dcbbf25
Hardik Gupta <hardikgupta0002@gmail.com>
2025-12-22 10:12:16 +0530

    added: register endpoint and optimized it with minor issue resolved

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,7 +7,7 @@ const app = new Elysia();
 
 registerFileRoutes(app, join(__dirname, ""))
 
-app.get("/", ({ redirect }) => {
+app.get("/", ({ redirect }: { redirect: (url: string) => void }) => {
   const url: string = process.env.MAIN_WEBSITE_URL || "";
   if (!url?.length) return "OK"
   return redirect(url);

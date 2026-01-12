c4a5e782a3291008e324843ee1df0895a6054f32
Hardik Gupta <hardikgupta0002@gmail.com>
2026-01-12 17:01:37 +0530

    fixed: removed teplates and added validate email checks

--- src/_user/index.ts
+++ src/_user/index.ts
@@ -5,16 +5,19 @@ import { UserFields } from "../../types/users/middleware"
 export default {
     use: (app: Elysia) => {
         return app.derive(async (context: Context) => {
-            const result = await validateLogin(context)
             if (process.env.UNAUTHORIZED_BYPASS_ENABLED !== "true" && context.request.headers.get("origin") !== process.env.MAIN_WEBSITE_URL) {
                 context.set.status = 401;
                 throw new Error("Unauthorized");
             }
-            
+            const result = await validateLogin(context)
+            if (!result.device.user.emailVerified) {
+                context.set.status = 401;
+                throw new Error("Email not verified");
+            }
             return {
                 userId: result.device.user.id,
                 user: result.device.user,
-                device: result.device
+                device: result.device,
             } satisfies UserFields
         })
     },
